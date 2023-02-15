8123df9ba10cfd8b280e4220c581962b9fdcf2fc
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2023-02-15 14:19:18 +0200

    add 01 code

--- 01_intro/01.R
+++ 01_intro/01.R
@@ -1,4 +1,113 @@
 2 + 2
 5 - 3 * 25
 
-5^5
+25 / 5
+
+3 ^ 3
+
+# залишок від ділення
+5 %% 3
+
+16 %/% 5 # цілочис. ділення
+
+(2 + 3) * (4 / 5)
+
+16 ^ 0.5
+16 ^ (1/2)
+sqrt(16)
+
+abs(3 - 10)
+
+exp(log(15))
+
+log(x = 10, base = 3)
+log(10, 3)
+log(3, 10)
+
+# # # # # # # # # # # # # # # # # # # # # # # 
+
+two = 2
+two
+
+one <- 1
+one + two
+
+5 -> five
+five
+
+# # # # # # # # # # # # # # # # # # # # # # # 
+
+one > two
+one < two
+one >= two
+one <= two
+one == two
+one != two
+
+# # # # # # # # # # # # # # # # # # # # # # # 
+
+class(one)
+typeof(one)
+is.double(one)
+
+one <- 1L
+typeof(one)
+is.double(one)
+
+
+one_ring <- "Ash nazg durbatulûk, ash nazg gimbatul, ash nazg thrakatulûk, agh burzum-ishi krimpatul."
+
+one_text <- '1'
+typeof(one_text)
+
+
+TRUE
+FALSE
+typeof(TRUE)
+
+# # # # # # # # # # # # # # # # # # # # # # # 
+
+vector_1 <- 1:5
+vector_1
+
+vector_2 <- c('one', 'two', 'five')
+vector_2
+
+vector_3 <- c(TRUE, FALSE, TRUE)
+vector_3
+
+vector_4 <- c(1:3)
+vector_4
+
+-5:5
+
+5:-5
+
+seq(from = 1, to = 100, length.out = 145)
+seq_vec <- seq(1, 100, length.out = 45)
+seq_vec
+
+c(3, 3, 3, 3, 3)
+rep(x = 3, times = 5)
+rep(3, 10)
+
+rep(1:5, 5)
+
+rep(1:3, 1:3)
+
+c(1, FALSE, "text")
+
+c(1, FALSE)
+
+five + TRUE
+
+vector_1 - 2
+
+(vector_1 + vector_4 * vector_1) / vector_4
+
+
+vector_4 + 5:10
+
+
+
+
